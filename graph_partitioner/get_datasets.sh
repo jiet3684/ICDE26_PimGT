7b1f4a4b8ab57f5da8f01205a49cc901165dc9e3
jiet3684 <43600272+jiet3684@users.noreply.github.com>
2025-06-19 19:02:26 +0900

    Fix

--- graph_partitioner/get_datasets.sh
+++ graph_partitioner/get_datasets.sh
@@ -2,40 +2,10 @@
 
 mkdir dataset
 
-wget https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/delaunay_n18.tar.gz
-tar -zxvf delaunay_n18.tar.gz
-mv delaunay_n18/delaunay_n18.mtx ./dataset
-rm -r delaunay_n18 delaunay_n18.tar.gz
-
-wget https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/delaunay_n20.tar.gz
-tar -zxvf delaunay_n20.tar.gz
-mv delaunay_n20/delaunay_n20.mtx ./dataset
-rm -r delaunay_n20 delaunay_n20.tar.gz
-
-wget https://suitesparse-collection-website.herokuapp.com/MM/QLi/largebasis.tar.gz
-tar -zxvf largebasis.tar.gz
-mv largebasis/largebasis.mtx ./dataset
-rm -r largebasis largebasis.tar.gz
-
 wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_indef/mario002.tar.gz
 tar -zxvf mario002.tar.gz
 mv mario002/mario002.mtx ./dataset
 rm -r mario002 mario002.tar.gz
-
-wget https://suitesparse-collection-website.herokuapp.com/MM/Botonakis/thermomech_dM.tar.gz
-tar -zxvf thermomech_dM.tar.gz
-mv thermomech_dM/thermomech_dM.mtx ./dataset
-rm -r thermomech_dM thermomech_dM.tar.gz
-
-wget https://suitesparse-collection-website.herokuapp.com/MM/Pajek/patents_main.tar.gz
-tar -zxvf patents_main.tar.gz
-mv patents_main/patents_main.mtx ./dataset
-rm -r patents_main patents_main.tar.gz
-
-wget https://suitesparse-collection-website.herokuapp.com/MM/SNAP/com-Amazon.tar.gz
-tar -zxvf com-Amazon.tar.gz
-mv com-Amazon/com-Amazon.mtx ./dataset
-rm -r com-Amazon com-Amazon.tar.gz
 
 wget https://suitesparse-collection-website.herokuapp.com/MM/SNAP/com-Youtube.tar.gz
 tar -zxvf com-Youtube.tar.gz
@@ -47,8 +17,24 @@ tar -zxvf thermal2.tar.gz
 mv thermal2/thermal2.mtx ./dataset
 rm -r thermal2 thermal2.tar.gz
 
-wget https://suitesparse-collection-website.herokuapp.com/MM/LAW/in-2004.tar.gz
-tar -zxvf in-2004.tar.gz
-mv in-2004/in-2004.mtx ./dataset
-rm -r in-2004 in-2004.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/333SP.tar.gz
+tar -zxvf 333SP.tar.gz
+mv 333SP/333SP.mtx ./dataset
+rm -r 333SP 333SP.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/NLR.tar.gz
+tar -zxvf NLR.tar.gz
+mv NLR/NLR.mtx ./dataset
+rm -r NLR NLR.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/delaunay_n22.tar.gz
+tar -zxvf delaunay_n22.tar.gz
+mv delaunay_n22/delaunay_n22.mtx ./dataset
+rm -r delaunay_n22 delaunay_n22.tar.gz
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/DIMACS10/rgg_n_2_23_s0.tar.gz
+tar -zxvf rgg_n_2_23_s0.tar.gz
+mv rgg_n_2_23_s0/rgg_n_2_23_s0.mtx ./dataset
+rm -r rgg_n_2_23_s0 rgg_n_2_23_s0.tar.gz
+
 
